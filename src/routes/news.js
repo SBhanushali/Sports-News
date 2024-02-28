1564abd9fad39700ddbb4ef9e8dfe1adeff808e5
Shivam Navin Bhanushali <shivam.b@grabtaxi.com>
2024-02-28 12:46:32 +0530

    [Problem 3] Document the contract details in comments

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,6 +1,18 @@
 const News = require("../controllers/news");
 
 module.exports = function (app) {
+  /**
+   * Endpoint to create a new news item.
+   *
+   * POST /api/news
+   * Creates a new news item with the provided title, description, and optional tourId or matchId.
+   *
+   * @param {string} req.body.title - The title of the news item.
+   * @param {string} req.body.description - The description of the news item.
+   * @param {string} [req.body.tourId] - The ID of the tour associated with the news item (optional).
+   * @param {string} [req.body.matchId] - The ID of the match associated with the news item (optional).
+   * @returns {object} Created news item object.
+   */
   app.route("/news").post(async (req, res, next) => {
     try {
       const data = req.body;
@@ -10,6 +22,15 @@ module.exports = function (app) {
     }
   });
 
+  /**
+   * Endpoint to retrieve news items by match ID.
+   *
+   * GET /api/news/match/:matchId
+   * Retrieves news items associated with the specified match ID.
+   *
+   * @param {number} req.params.matchId - The ID of the match for which to retrieve news items.
+   * @returns {object[]} An array of news items associated with the specified match ID.
+   */
   app.route("/news/match/:matchId").get(async (req, res, next) => {
     try {
       const matchId = parseInt(req.params.matchId);
@@ -19,6 +40,15 @@ module.exports = function (app) {
     }
   });
 
+  /**
+   * Endpoint to retrieve news items by tour ID.
+   *
+   * GET /api/news/tour/:tourId
+   * Retrieves news items associated with the specified tour ID.
+   *
+   * @param {number} req.params.tourId - The ID of the tour for which to retrieve news items.
+   * @returns {object[]} An array of news items associated with the specified tour ID.
+   */
   app.route("/news/tour/:tourId").get(async (req, res, next) => {
     try {
       const tourId = parseInt(req.params.tourId);
@@ -28,6 +58,15 @@ module.exports = function (app) {
     }
   });
 
+  /**
+   * Endpoint to retrieve news items by sport ID.
+   *
+   * GET /api/news/sport/:sportId
+   * Retrieves news items associated with the specified sport ID.
+   *
+   * @param {number} req.params.sportId - The ID of the sport for which to retrieve news items.
+   * @returns {object[]} An array of news items associated with the specified sport ID.
+   */
   app.route("/news/sport/:sportId").get(async (req, res, next) => {
     try {
       const sportId = parseInt(req.params.sportId);
